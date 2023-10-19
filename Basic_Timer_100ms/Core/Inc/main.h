f4e4847250aff9cdef805092dd0ad459d86d5664
Nemanja Djekic <nemanja.dev@skiff.com>
2023-10-19 12:57:42 +0200

    Migrate to STM32Cube FW_f4 V1.27.1

--- Basic_Timer_100ms/Core/Inc/main.h
+++ Basic_Timer_100ms/Core/Inc/main.h
@@ -1,13 +1,49 @@
-/*
- * main.h
- *
- *  Created on: Dec 24, 2020
- *      Author: nemanja
- */
 
-#ifndef INC_MAIN_H_
-#define INC_MAIN_H_
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include "stm32f4xx_hal.h"
 
-#endif /* INC_MAIN_H_ */
+void Error_Handler(void);
+
+#define B1_GPIO_Port GPIOC
+#define USART_TX_Pin GPIO_PIN_2
+#define USART_TX_GPIO_Port GPIOA
+#define USART_RX_Pin GPIO_PIN_3
+#define USART_RX_GPIO_Port GPIOA
+#define LD2_Pin GPIO_PIN_5
+#define LD2_GPIO_Port GPIOA
+#define TMS_Pin GPIO_PIN_13
+#define TMS_GPIO_Port GPIOA
+#define TCK_Pin GPIO_PIN_14
+#define TCK_GPIO_Port GPIOA
+#define SWO_Pin GPIO_PIN_3
+#define SWO_GPIO_Port GPIOB
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
